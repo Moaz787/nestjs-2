e5c27eadba94be5fd092a92f9a6878a31a305fde
moaz <moaazseadawy@gmail.com>
2025-07-04 12:18:06 +0300

    setup orm, configrution

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,31 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ProductsModule,
     ProductsController,
     ProductsService,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // Set to false in production
+      }),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.development',
+    }),
   ],
 })
 export class AppModule {}
